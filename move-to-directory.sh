d07f914bc3251de9b433bcd5ba6bbed1b7a2ade9
EliseJulio <j.hakiziman1@alustudent.com>
2024-06-13 15:08:18 -0700

    Touch of a Genius

--- move-to-directory.sh
+++ move-to-directory.sh
@@ -1,2 +1,2 @@
 # Define the files to be moved
-mv main.sh students-list_1023.txt select-emails.sh student-emails.txt backup-Negpod_10.sh negpod_10-q1/
+mv main.sh students-list_0524.txt select-emails.sh student-emails.txt backup-Negpod_10.sh negpod_10-q1/
