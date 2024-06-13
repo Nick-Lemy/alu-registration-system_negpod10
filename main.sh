cf83466e7b625594232d02bea9cf1051ce0b67d5
CletusAAbugre <c.abugre@alustudent.com>
2024-06-13 13:09:32 -0700

    fun

--- main.sh
+++ main.sh
@@ -96,7 +96,7 @@ menu() {
     echo "4. Delete Student"
     echo "5. Save Student Email Sorted in ASC"
     echo "6. Back-up your data"
-    echo "6. Exit"
+    echo "7. Exit"
     echo "----------------------------------"
     echo "Enter your choice (1-6):"
     read choice
